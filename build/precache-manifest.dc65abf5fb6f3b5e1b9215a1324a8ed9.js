f2aa7e3a7545a6ade52f07a6b2a9d15daa78d1e2
Ashish Pradhan <ashish_s98@hotmail.com>
2020-03-05 19:41:16 -0500

    counter app

---
+++ build/precache-manifest.dc65abf5fb6f3b5e1b9215a1324a8ed9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe5566959613c8499a10",
+    "url": "/static/js/main.93eb7b04.chunk.js"
+  },
+  {
+    "revision": "2bf7d2a4c5537bb1786e",
+    "url": "/static/js/2.85a3da2e.chunk.js"
+  },
+  {
+    "revision": "b8935e3657dd601cef13d84d35c46279",
+    "url": "/index.html"
+  }
+];
